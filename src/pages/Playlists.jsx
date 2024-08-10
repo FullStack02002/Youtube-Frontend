e494e33b489a0bf56e7d2c5e337f09425ac9a7e0
Gaj Singh Rathore <gajrathore353@gmail.com>
2024-08-11 02:12:45 +0530

    added playlispageskeleton

--- src/pages/Playlists.jsx
+++ src/pages/Playlists.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getUserPlaylist } from "../store/Slices/playlistSlice";
-
+import { BigLoader } from "../components";
+import { CgPlayList } from "../components/icons";
+import PlaylistPageSkeleton from "../skeletons/PlaylistPageSkeleton";
 const Playlists = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const userId = useSelector((state) => state.auth?.userData?._id);
   const playlists = useSelector((state) => state.playlist?.playlists) || [];
+  const loading = useSelector((state) => state.playlist?.loading);
 
   useEffect(() => {
     if (userId) {
@@ -15,9 +18,15 @@ const Playlists = () => {
     }
   }, [userId]);
 
+  if (loading) {
+    return <PlaylistPageSkeleton></PlaylistPageSkeleton>
+  }
+
   return (
     <div className="text-white pt-5 sm:p-5">
-      <h1 className="text-3xl font-bold mb-4 sm:text-start ml-0 sm:ml-4 text-center">Playlists</h1>
+      <h1 className="text-3xl font-bold mb-4 sm:text-start ml-0 sm:ml-4 text-center">
+        Playlists
+      </h1>
       <div className="flex flex-col gap-1  md:flex md:flex-row flex-wrap  md:gap-20 lg:gap-10">
         {playlists.map((item) => {
           const thumbnail = item.videos?.[0]?.thumbnail;
@@ -40,6 +49,15 @@ const Playlists = () => {
                   src={thumbnail}
                   className="w-full rounded-xl h-[166.8px]"
                 />
+              </div>
+
+              <div className=" absolute bottom-[70px] right-[30px] z-10  bg-[#00000099] flex flex-row items-center p-2 gap-1">
+                <CgPlayList className="text-white font-bold" size={20} />
+                <span className="text-white text-[14px]">
+                  {item.totalVideos
+                    ? `${item.totalVideos} videos`
+                    : "No videos"}
+                </span>
               </div>
 
               <div className=" mt-[140px] w-full">
